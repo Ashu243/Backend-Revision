6dca7571757020067e746b5f5fc94ae7e4d0f276
Ashu <ashuv6309@gmail.com>
2026-08-02 15:44:17 +0530

    practice sql queries like joins, group by etc

--- Postgres_CRUD/data.sql
+++ Postgres_CRUD/data.sql
@@ -14,10 +14,10 @@ add city varchar(50);
 -- Challenge 2: Insert Data
 insert into users (name, email, password, age, city)
 values 
-('Ashu', 'ashu@gmail.com', 'ashu', 20, 'Gurgaon'),
-('Rahul', 'rahul@gmail.com', 'Rahul', 23, 'Delhi'),
-('Priya', 'priya@gmail.com', 'Priya', 25, 'Mumbai'),
-('Aman', 'aman@gmail.com', 'Aman', 27, 'Goa');
+('Deep', 'Deep@gmail.com', 'Deep', 15, 'Gurgaon'),
+('vandana', 'vandana@gmail.com', 'vandana', 43, 'Delhi'),
+('Seema', 'Seema@gmail.com', 'Seema', 25, 'Banglore'),
+('palak', 'palak@gmail.com', 'palak', 17, 'Gurgaon');
 
 -- Challenge 3: Retrieve Data
 
@@ -151,3 +151,170 @@ where name not like 'Am%'
 and name like 'A%';
 
 
+-- Count
+SELECT COUNT(*) FROM users;
+
+-- count city
+select count(city) from users;
+
+
+-- avg
+select avg(age) from users;
+
+-- max
+select max(age) from users;
+
+-- min 
+select min(age) from users;
+
+-- distinct - return unique
+select distinct city from users;
+
+select count(distinct city) from users; -- returns count of distinct city
+
+
+-- Group by
+select city, count(*) as people_living
+from users
+group by city;
+
+-- show city where more than 1 person lives
+select city, count(*)
+from users
+group by city
+having count(*)>1; -- why not (where)? because where works on rows not groups. Having works on groups
+
+
+-- GROUP BY
+-- Count users in each city.
+select city, count(*) as people_living
+from users
+group by city;
+
+-- Find the average age in each city.
+select city, avg(age) as average_age
+from users
+group by city;
+
+-- Find the maximum age in each city.
+select city, max(age) as max_age
+from users
+group by city;
+
+-- Find the minimum age in each city.
+select city, min(age) as min_age
+from users
+group by city;
+
+
+-- HAVING
+-- Show cities having more than 1 user.
+select city, count(*)
+from users
+group by city
+having count(*)>1;
+
+-- Show cities where the average age is greater than 22.
+select city, avg(age)
+from users
+group by city
+having avg(age)>22;
+
+-- Show cities having exactly 2 users.
+select city, count(*)
+from users
+group by city
+having count(*)=2;
+
+-- Can you write one query that returns:
+
+-- City	    Users	Average Age
+-- Delhi	3	    23
+-- Goa	    2	    24.5
+
+select city, count(*) as Users, avg(age) as Average_age
+from users
+group by city;
+
+
+
+
+-- creating orders table
+create table orders(
+    id serial primary key,
+    user_id int,
+    product varchar(100) not null,
+
+    constraint fk_user
+    foreign key (user_id)
+    references users(id)
+);
+
+alter table orders
+add amount int;
+
+insert into orders (user_id, product)
+values
+(2, 'laptop'),
+(4, 'Shirt'),
+(5, 'books'),
+(6, 'phone'),
+(7, 'pen');
+
+
+-- INNER JOIN - only returns matching rows from both the table
+select users.name, orders.product -- columns to show in result
+from users -- left table
+inner join orders -- right table
+on users.id = orders.user_id; -- condition to match both the tables
+
+-- LEFT JOIN - return all the rows from left table and matching rows from right table
+select u.name, o.product
+from users u -- here u is alias
+left join orders o
+on u.id = o.user_id;
+
+-- RIGHT JOIN - return matching rows from left table and all the rows from right table
+select users.name, orders.product
+from users
+right join orders
+on users.id = orders.user_id;
+
+-- FULL OUTER JOIN - return all the rows from left and right table if they match or not
+select users.name, orders.product
+from users
+full outer join orders
+on users.id = orders.user_id;
+
+
+
+
+-- Write a query to show:
+-- Name	Total Orders
+-- Ashu	2
+-- Rahul	1
+-- Aman	0
+-- Neha	1
+-- Priya	0
+
+select users.name, count(orders.id) as total_orders
+from users
+left join orders
+on users.id = orders.user_id
+group by users.name;
+
+
+-- Show every user and the total amount they have spent.
+-- Expected output:
+-- Name	Total Spent
+-- Ashu	1700
+-- Rahul	60000
+-- Aman	0
+-- Neha	15000
+-- Priya	0
+
+select users.name, COALESCE(sum(orders.amount), 0)
+from users
+left join orders
+on users.id = orders.user_id
+group by users.name;
